076d7aed5c2e591c5fae2ce33f69d938e25ac3b0
Mohamed Miguil <mi.gu@hotmail.co.uk>
2025-05-21 14:33:31 +0100

    home page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@rails/ujs";     
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
 import flatpickr from "flatpickr"
 flatpickr(".datepicker", {})
+
+Rails.start();
